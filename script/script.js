0440b8f3c1ec4d71c90e1f2ec9767d04ae910842
Carlos Felipe <109633306+Felipe-Monte@users.noreply.github.com>
2022-11-23 09:02:52 -0300

    update

--- script/script.js
+++ script/script.js
@@ -1,22 +1,28 @@
+function myScope() {
+    const list = document.querySelectorAll('li')
+    const submit = document.querySelector('.btn')
+    const secondCard = document.querySelector('.container-second-card')
+    const btnBack = document.querySelector('.btn-second')
+    const textSpan = document.querySelector('.result')
 
-const list = document.querySelectorAll('li')
-const submit = document.querySelector('.btn')
-const secondCard = document.querySelector('.container-second-card')
-const textSpan = document.querySelector('.result')
+    for (let i = 0; i < list.length; i++) {
+        const item = list[i]
+        item.addEventListener('click', function (e) {
+            for (let j = 0; j < list.length; j++) {
+                const color = list[j] === item ? '#FC7614' : '#262E38'
+                list[j].style.backgroundColor = color
+            }
+            textSpan.innerHTML = item.value
 
+        })
+    }
 
-for (let i = 0; i < list.length; i++) {
-   const item = list[i]
-   item.addEventListener('click' , function(){
-       for (let j = 0; j < list.length; j++) {
-           const color = list[j] === item ? '#FC7614' : '#262E38'
-           list[j].style.backgroundColor = color
-       }
-       
-       textSpan.innerHTML = item.value
-   })
+    submit.addEventListener('click', function (e) {
+        secondCard.style.display = 'block'
+    })
+
+    btnBack.addEventListener('click', function (e) {
+        secondCard.style.display = 'none'
+    })
 }
-
-submit.addEventListener('click' , function(){
-    secondCard.style.display = 'block' 
-})
+myScope()
