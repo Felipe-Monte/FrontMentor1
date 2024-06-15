32ebfd20cd76f761471c633608e5fbcbba5361d0
felipe <carlosfelipemonte1997@hotmail.com>
2024-06-15 10:29:21 -0300

    refactoring

---
+++ script/index.js
@@ -0,0 +1,9 @@
+//REFACTORING
+document.addEventListener("DOMContentLoaded", function () {
+  const allOptionsLi = document.querySelectorAll("li")
+  
+  allOptionsLi.forEach(option => {
+    console.log(option)
+  })
+
+})
