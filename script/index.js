8e6c6491fbfad59da6171d808bc1505bdf2a32a6
Carlos Felipe <carlosfelipemonte1997@outlook.com>
2024-06-17 19:19:45 -0300

    refactored complete

--- script/index.js
+++ script/index.js
@@ -1,21 +1,36 @@
+//refactoclicked
 document.addEventListener("DOMContentLoaded", function () {
   const allOptionsLi = document.querySelectorAll("li");
-  const btnSubmit = document.querySelector(".btn")
+  const secondCard = document.querySelector(".container-second-card");
+  const numberVoted = document.querySelector(".result");
+  const btnSubmit = document.querySelector(".btn");
+  const btnBack = document.querySelector(".btn-second");
 
   allOptionsLi.forEach((option) => {
     option.addEventListener("click", function () {
-      allOptionsLi.forEach((opt) => opt.classList.remove("red"));
-      this.classList.add("red");
+      allOptionsLi.forEach((opt) => opt.classList.remove("clicked"));
+      this.classList.add("clicked");
     });
   });
 
   btnSubmit.addEventListener("click", function () {
+    let isVoted = false;
+
     allOptionsLi.forEach((opt) => {
-      if(opt.classList.contains('red')){
-        console.log(opt)
+      if (opt.classList.contains("clicked")) {
+        numberVoted.textContent = opt.value;
+        secondCard.style.display = "block";
+        isVoted = true;
       }
-    })
-  })
+    });
+
+    if (!isVoted) {
+      alert("Por favor deixe seu voto!");
+    }
+  });
+
+  btnBack.addEventListener("click", function () {
+    allOptionsLi.forEach((opt) => opt.classList.remove("clicked"));
+    secondCard.style.display = "none";
+  });
 });
-
-
